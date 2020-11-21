a3993e4a2b405f4bf7bb1aa5d45cf3d854a65fb9
Andrii Butko <andrii.butko@eleks.com>
2020-11-21 12:42:18 +0200

    Implement base stream server over grpc

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/andreybutko/fibonacci
 
 go 1.15
+
+require (
+	google.golang.org/grpc v1.33.2
+	google.golang.org/protobuf v1.25.0
+)
--- server/server.go
+++ server/server.go
@@ -1,5 +1,44 @@
 ﻿package main
 
-func main() {
+import (
+	"github.com/andreybutko/fibonacci/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
 
+type FibonacciServer struct {
+	proto.UnimplementedFibonacciServer
+}
+
+const (
+	PORT = "9002"
+)
+
+func (s *FibonacciServer) GetSequence(_ *proto.FibonacciRequest, stream proto.Fibonacci_GetSequenceServer) error {
+	for n := 0; n <= 6; n++ {
+		err := stream.Send(&proto.FibonacciReply{
+			Message: int64(n),
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	server := grpc.NewServer()
+	proto.RegisterFibonacciServer(server, &FibonacciServer{})
+
+	lis, err := net.Listen("tcp", ":"+PORT)
+	if err != nil {
+		log.Fatalf("net.Listen err: %v", err)
+	}
+
+	serverr := server.Serve(lis)
+	if serverr != nil {
+		log.Fatalf("Server error: %v", serverr)
+	}
 }
